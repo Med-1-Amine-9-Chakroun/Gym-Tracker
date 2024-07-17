517d1c562028f3ff4793bdaa761c0b1a82aa6ed5
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-17 09:45:37 +0100

    Folder routes created, workout router file created

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const workoutRoutes = require("./routes/workouts");
 //express app
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// listen for requests
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
