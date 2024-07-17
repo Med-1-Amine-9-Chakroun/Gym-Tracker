096cda68ccb620470dc62d709e10938e93bcc926
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-17 15:22:10 +0100

    axios and cors used instead of proxy

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
-
+app.use(cors());
 // middleware
+
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      axios
+        .get("http://localhost:3000/api/workouts")
+        .then((result) => {
+          setWorkouts(result.data.workouts);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
     </div>
   );
 }
