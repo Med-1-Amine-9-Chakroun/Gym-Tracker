cc66c5368d229e69d45b8494ac1ad4b4c6296a27
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-17 12:03:00 +0100

    home page and navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Tracker</h1>
+        </Link>
+      </div>
+    </header>
+  );
+}
