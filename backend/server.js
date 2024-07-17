17ffba4558e9dcd9ef2da3710b6d312b1abb53fc
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-17 10:21:03 +0100

    Connexion to DB configured

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+
 //express app
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -14,7 +17,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONG_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB & listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
